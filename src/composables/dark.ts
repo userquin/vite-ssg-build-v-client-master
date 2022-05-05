9c1de30e7f011033d8ac78c82e7f5542606e6480
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2022-05-05 14:16:38 +0200

    add original `useDark`

--- src/composables/dark.ts
+++ src/composables/dark.ts
@@ -1,2 +1,3 @@
+// export const isDark = useDark()
 export const isDark = typeof window !== 'undefined' && typeof window.matchMedia !== 'undefined' ? useDark() : ref(false)
 export const toggleDark = useToggle(isDark)
